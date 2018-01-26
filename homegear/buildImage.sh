153fe72decc5a2613159d72391d11c4399584ba5
Psycho0verload <mail@psycho0verload.de>
2018-01-26 14:59:37 +0100

    Update buildImage.sh

--- homegear/buildImage.sh
+++ homegear/buildImage.sh
@@ -1,3 +1,5 @@
 #/bin/sh
 docker build --no-cache -t psycho0verload/armv7hf-lib:homegear .
 docker tag psycho0verload/armv7hf-lib:homegear psycho0verload/armv7hf-lib:homegear-latest
+docker pull psycho0verload/armv7hf-lib:homegear
+docker pull psycho0verload/armv7hf-lib:homegear-latest
